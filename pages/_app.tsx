e1f6e8f8f659ca86cc936a1c6d77c392a3bc7ee5
kyldex <raphael.giagnorio@gmail.com>
2023-02-14 23:17:24 +0100

    set up sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/layout/Layout';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
