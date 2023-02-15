c53189856ffa3c29ec73d29b98f491ea0d7d13ba
kyldex <raphael.giagnorio@gmail.com>
2023-02-15 01:50:35 +0100

    set up fonts

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,13 +2,15 @@ import Nav from './Nav';
 import Footer from './Footer';
 import styles from './Layout.module.scss';
 
+import { vercettiFont } from '../../utils/fontModule';
+
 interface Props {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className={styles.page}>
+    <div className={`${styles["page"]} ${vercettiFont.className}`}>
       <Nav />
       <main className={styles.main}>{children}</main>
       <Footer />
