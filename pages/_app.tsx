4a05eab7979c99ccca9b71b44ce020be80954f48
kyldex <raphael.giagnorio@gmail.com>
2024-06-11 00:27:29 +0200

    feat(analytics): set up environments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,22 +8,12 @@ import '../styles/globals.scss';
 import { DeviceProvider } from '../context/DeviceContext';
 import Layout from '../components/layout/Layout';
 
+import Analytics from '../components/analytics';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-1LSW9T6KFY"
-      />
-      <Script id="analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-1LSW9T6KFY');
-        `}
-      </Script>
+      <Analytics />
 
       <DeviceProvider>
         <Layout>
