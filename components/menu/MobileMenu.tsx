90d8052e3ba4f9274de1be353eb18d1b5d4ac6e9
kyldex <raphael.giagnorio@gmail.com>
2024-06-21 17:29:11 +0200

    feat(categories): add subcategories in mobile menu

--- components/menu/MobileMenu.tsx
+++ components/menu/MobileMenu.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { CaretDownIcon } from '@radix-ui/react-icons';
 
 import styles from './MobileMenu.module.scss';
 
+import MenuLink from './common/MenuLink';
+
+import { CATEGORIES, SUBCATEGORIES } from '../../utils/constants';
 import closeIcon from '../../assets/img/menu/close.png';
 import orangeStar from '../../assets/img/menu/star_orange.svg';
 
-interface Props {
+type Props = {
   toggleMenuIsOpen: () => void;
-}
+};
 
 const MobileMenuBar: FC<Props> = ({ toggleMenuIsOpen }) => {
   return (
@@ -34,49 +38,166 @@ const MobileMenuBar: FC<Props> = ({ toggleMenuIsOpen }) => {
               alt=""
             />
 
-            <ul id="mobile-menu" className={styles.mobileMenu}>
-              <li>
-                <Link href="/" onClick={toggleMenuIsOpen}>
-                  ACCUEIL
-                </Link>
-              </li>
-              <li>
-                <Link href="/evenements" onClick={toggleMenuIsOpen}>
-                  ÉVÈNEMENTS
-                </Link>
-              </li>
-              <li>
-                <Link href="/articles" onClick={toggleMenuIsOpen}>
-                  ARTICLES
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/production-audiovisuelle"
-                  onClick={toggleMenuIsOpen}
-                >
-                  PRODUCTION AUDIOVISUELLE
-                </Link>
-              </li>
-              <li>
-                <Link href="/contributeurs" onClick={toggleMenuIsOpen}>
-                  CONTRIBUTEURS
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/observatoire-des-perceptions"
-                  onClick={toggleMenuIsOpen}
-                >
-                  OBSERVATOIRE DES PERCEPTIONS
-                </Link>
-              </li>
-              <li>
-                <Link href="/apropos" onClick={toggleMenuIsOpen}>
-                  À PROPOS
-                </Link>
-              </li>
-            </ul>
+            <NavigationMenu.Root
+              orientation="vertical"
+              className={styles.navigationMenuRoot}
+            >
+              <NavigationMenu.List className={styles.navigationMenuList}>
+              <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <MenuLink
+                    href="/"
+                    className={styles.mainMenuLink}
+                    onClick={toggleMenuIsOpen}
+                  >
+                    ACCUEIL
+                  </MenuLink>
+                </NavigationMenu.Item>
+
+                <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <MenuLink
+                    href="/evenements"
+                    className={styles.mainMenuLink}
+                    onClick={toggleMenuIsOpen}
+                  >
+                    {CATEGORIES.EVENTS.toUpperCase()}
+                  </MenuLink>
+                </NavigationMenu.Item>
+
+                <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <NavigationMenu.Trigger
+                    className={styles.navigationMenuTrigger}
+                  >
+                    {CATEGORIES.ARTICLES.toUpperCase()}
+                    <CaretDownIcon className={styles.caretDown} aria-hidden />
+                  </NavigationMenu.Trigger>
+                  <NavigationMenu.Content
+                    className={styles.navigationMenuContent}
+                  >
+                    <ul className={`${styles.contentList} ${styles.articles}`}>
+                      <li>
+                        <MenuLink
+                          href="/articles/actualites"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.NEWS}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/articles/idees"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.IDEAS}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/articles/sommes-nous-tous-fous"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.CRAZY}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/articles/numerique"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.DIGITAL}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/articles/culture"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.CULTURE}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/articles/societe"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.SOCIETY}
+                        </MenuLink>
+                      </li>
+                    </ul>
+                  </NavigationMenu.Content>
+                </NavigationMenu.Item>
+
+                <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <NavigationMenu.Trigger
+                    className={styles.navigationMenuTrigger}
+                  >
+                    {CATEGORIES.AUDIOVISUAL_PRODUCTION.toUpperCase()}
+                    <CaretDownIcon className={styles.caretDown} aria-hidden />
+                  </NavigationMenu.Trigger>
+                  <NavigationMenu.Content
+                    className={styles.navigationMenuContent}
+                  >
+                    <ul
+                      className={`${styles.contentList} ${styles.audiovisual}`}
+                    >
+                      <li>
+                        <MenuLink
+                          href="/production-audiovisuelle/portraits-de-francais"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.FRENCH_PORTRAITS}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/production-audiovisuelle/interviews"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.INTERVIEWS}
+                        </MenuLink>
+                      </li>
+                      <li>
+                        <MenuLink
+                          href="/production-audiovisuelle/contributions-externes"
+                          onClick={toggleMenuIsOpen}
+                        >
+                          {SUBCATEGORIES.EXTERNAL_CONTRIBUTIONS}
+                        </MenuLink>
+                      </li>
+                    </ul>
+                  </NavigationMenu.Content>
+                </NavigationMenu.Item>
+
+                <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <MenuLink
+                    href="/contributeurs"
+                    className={styles.mainMenuLink}
+                    onClick={toggleMenuIsOpen}
+                  >
+                    CONTRIBUTEURS
+                  </MenuLink>
+                </NavigationMenu.Item>
+
+                <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <MenuLink
+                    href="/observatoire-des-perceptions"
+                    className={styles.mainMenuLink}
+                    onClick={toggleMenuIsOpen}
+                  >
+                    OBSERVATOIRE DES PERCEPTIONS
+                  </MenuLink>
+                </NavigationMenu.Item>
+
+                <NavigationMenu.Item className={styles.navigationMenuItem}>
+                  <MenuLink
+                    href="/apropos"
+                    className={styles.mainMenuLink}
+                    onClick={toggleMenuIsOpen}
+                  >
+                    À PROPOS
+                  </MenuLink>
+                </NavigationMenu.Item>
+              </NavigationMenu.List>
+            </NavigationMenu.Root>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
